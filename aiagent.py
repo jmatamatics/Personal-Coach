ff8ef48fdf905d0207e88f509da399ad06c2fef2
Joseph Mata <32107848+jmatamatics@users.noreply.github.com>
2023-09-17 12:15:50 -0400

    Add files via upload

---
+++ aiagent.py
@@ -0,0 +1,53 @@
+import openai
+import streamlit
+
+openai.api_key = streamlit.secrets['OPENAI_API_KEY']
+    
+class AIAgent():
+    def __init__(self, model="gpt-3.5-turbo"):
+        """Initialize the AI Agent.  Set the system message, initial prompt prefix, and initial response"""
+        self.model=model
+        self.system_message = """For each query, consider writings of Saint Germaine that have addressed that question and choose one.
+        Respond to the query from the point of view of that philosopher
+        Finally, use those referenced sources to form a response to the query that the chosen philosopher would support.
+        Your response should be written from the point of view of that philosopher and be sure your response is supported by original sources
+        of text authored by that philosopher.  You should reference those sources in your response.
+        For example:
+        Query: What is the meaning of life?
+        Response: Hi, my name is Albert Camus.  In The Myth of Sisyphus, I wrote about how life is meaningless from
+        an objective viewpoint, but that we can create our own meaning for our lives.  In my works of fiction, I describe
+        situations where people must make their own meaning in during times of crisis, doubt and confusion.
+        Ultimately, life is what you make of it and it means what it means to you.  One of my most famous quotes is:
+        "The meaning of life is whatever you are doing that keeps you from killing yourself."""
+        self.prefix = ''
+        self.history = [{'role': 'system', 'content':self.system_message}]
+        self.response = ''
+        
+    def add_message(self, text, role):
+        """Add a message to the conversation history"""
+        message = {'role':role, 'content':text}
+        self.history.append(message)
+        
+    def query(self, prompt, temperature=.1):
+        # Add user prompt to history
+        self.add_message(self.prefix + prompt, 'user')
+
+        # Query the model through the API 
+        result = openai.ChatCompletion.create(
+            model=self.model,
+            messages=self.history,
+            temperature=temperature, # this is the degree of randomness of the model's output
+        )
+
+        self.response = result.choices[0].message["content"]
+        # Add reply to message history
+        self.add_message(self.response, 'assistant')
+        
+        # set prompt prefix to ensure same philosopher answers each time.
+        self.prefix = 'Please continue to respond as the previous philosopher: '
+           
+    def clear_history(self):
+        """Reset the history to its initial state.  Also reset the prefix and current response"""
+        self.history = [{'role':'system', 'content':self.system_message}]
+        self.prefix = ''
+        self.response = ''
---
+++ app.py
@@ -0,0 +1,73 @@
+import streamlit as st
+from aiagent import AIAgent
+
+from streamlit.web.server.websocket_headers import _get_websocket_headers
+
+# Get session id
+headers = _get_websocket_headers()
+session_id = headers.get("Sec-Websocket-Key")
+
+@st.cache_resource
+def get_agent(session_id):
+    # Instantiate AIAgent if no cached agent is found.  
+    # This happens exactly one time each time a new session is started
+    agent = AIAgent(model='gpt-3.5-turbo')
+    print('creating the ai agent')
+    agent.reponse = 'We wont tell what to see. Only where to look.'
+    return agent
+
+def format_history(history):
+    # Make the history more readable
+    report = ''
+    for message in history[1:]:
+        if message['role'] == 'user':
+            report += (f"\n\nYOU: {message['content'].replace(agent.prefix, '')}")
+        else:
+            report += (f"\n\nPHILOSOPHER: {message['content']}")
+    return report
+ 
+def query_agent():
+    # Sent user query to agent
+    if prompt:
+        try:      
+            current_response.write("Reality Center is considering your query and will send a representative soon.")
+            agent.query(prompt, 
+                        temperature=temperature
+                                )
+        except Exception as e:
+            agent.response = "I'm sorry, all philosophers are busy helping other wisdom seekers.  Please try again later."
+            print(e)
+    else:
+        print('no prompt')
+
+# Page Header
+st.title('Chat with Saint Germaine')
+
+# User prompt input
+prompt = st.text_input(label="Ask a question and St Gremaine will respond",
+                       max_chars=1000,
+                       value='',
+                       help="Repsonses will source the material the resopnse was based on.",
+                       key='user_query',
+                       placeholder="Enter your burning question here")
+
+# Instantiate agent (only if it has not been created yet, otherwise use cached agent)
+agent = get_agent(session_id)
+
+# Submit user input to query agent.
+st.button('Submit your question to the Reality Center',
+          on_click=query_agent
+          )
+
+# Slider bar to adjust temperature on the sidebar
+temperature = st.sidebar.slider('Creativity', min_value=0.0, max_value=1.0, step=0.1, value=0.1)
+
+# button to clear the message history and start a new conversation on the sidebar
+st.sidebar.button('New conversation', on_click=agent.clear_history,
+                   use_container_width=False)
+
+# display the conversation so far on the sidebar
+st.sidebar.markdown(f'The conversation so far: {format_history(agent.history)}')
+
+# Display most recent response
+current_response = st.markdown(agent.response)
