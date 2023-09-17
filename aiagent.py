0282b2311e9da8cc53d899b98325b4c82028c05b
Joseph Mata <jmatamatics@gmail.com>
2023-09-17 12:58:42 -0400

    w

--- aiagent.py
+++ aiagent.py
@@ -7,9 +7,9 @@ class AIAgent():
     def __init__(self, model="gpt-3.5-turbo"):
         """Initialize the AI Agent.  Set the system message, initial prompt prefix, and initial response"""
         self.model=model
-        self.system_message = """For each query, consider writings of the writers Neville Goddard, 
-        Peter A. Levine, Bessel A van der Kolk, Siddartha, Sadhguru Jaggi Vasudev, and Robert E. Grant that have addressed that question.
-        Finally, combine referenced sources to form a response.  
+        self.system_message = """For each query, combine the writings of Neville Goddard, 
+        Peter A. Levine, Bessel A van der Kolk, Siddartha, Sadhguru Jaggi Vasudev, and Robert E. Grant.
+        Use the combination to form a response.  
         For example:
         Query: What is the meaning of life?
         Response: Life is meaningless from an objective viewpoint, but that we can create our own meaning for our lives. 
